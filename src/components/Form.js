f2ca9f2586af26dbf7cce85bf2881953fcb64599
Nick Allen <nwestallen@gmail.com>
2021-01-27 14:21:37 -0500

    submit adds member

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 //Form component
 import React, { useState, useEffect } from 'react';
-import sytled from 'styled-components';
+import styled from 'styled-components';
 import TeamList from './TeamList';
 
 const Form = (props) => {
@@ -9,17 +9,41 @@ const Form = (props) => {
     const [member, setMember] = useState({name: 'Frank', email: 'Frank@franknbeans.com', role: 'rhythm'});
 
     const handleChange = event => setMember({...member, [event.target.name]: event.target.value});
+    const handleSubmit = event => {
+        event.preventDefault();
+        setTeam([...team, member]);
+    };
+
+    const MemberForm = styled.form`
+        display: flex;
+        flex-flow: column nowrap;
+        width: 20%;
+        align-items: center;
+        margin: auto;
+        color: white;
+        border: 2px solid white;
+        button {
+            width: fit-content;
+        }
+    `;
 
     return (
-        <form>
+        <MemberForm onSubmit={handleSubmit}>
+            <h3>Add Member</h3>
+            <p>
             <label for='nameInput'>Name: </label>
             <input type='text' id='nameInput' name='name' onChange={handleChange} value={member.name}/>
+            </p>
+            <p>
             <label for='emailInput'>Email: </label>
             <input type='text' id='emailInput' name='email' onChange={handleChange} value={member.email}/>
-            <button>Submit</button>
+            </p>
+            <p>
             <label for='roleInput'>Role: </label>
             <input type='text' id='roleInput' name='role' onChange={handleChange} value={member.role}/>
-        </form>
+            </p>
+            <button>Submit</button>
+        </MemberForm>
     )
 }
 
